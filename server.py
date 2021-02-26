9d3cb31fa0621f193daa66000e167cbd1a91851e
Lucas N. Ferreira <lucasnfe@gmail.com>
2021-02-26 13:28:59 -0300

    Refactored code.

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ from flask import render_template
 
 from pymongo import MongoClient
 
+# Define host name
+sever_name="localhost:5000"
+
 # Connect with database
 database_url = "localhost:27017"
 database = MongoClient(database_url)
@@ -15,8 +18,10 @@ collection = database["user_study"]["jmlr20"]
 
 app = Flask(__name__)
 
-pieces = ["audio/piece1.mp3",
-          "audio/piece1.mp3",
+tutorial = "audio/piece1.mp3"
+
+pieces = [tutorial,
+          tutorial,
           "audio/piece2.mp3",
           "audio/piece3.mp3",
           "audio/piece4.mp3",
@@ -29,20 +34,25 @@ def page_not_found(e):
 @app.route('/')
 def index():
     # Create evaluation sequence
-    piece_sequence = [i+1 for i in range(len(pieces) - 1)]
+    piece_sequence = [i+2 for i in range(len(pieces) - 2)]
     random.shuffle(piece_sequence)
+
+    # Insert tutorial in the beginning and in the middle of the sequence
     piece_sequence.insert(0, 0)
+    piece_sequence.insert(len(piece_sequence)//2 + 1, 1)
 
     piece_sequence = ",".join([str(i) for i in piece_sequence])
     return render_template('index.html', piece_sequence=piece_sequence,
-                                           total_pieces=len(pieces))
+                                           total_pieces=len(pieces),
+                                             sever_name=sever_name)
 
 @app.route('/tutorial')
 def tutorial():
     return render_template('evaluate.html', piece_number=0,
                                               piece_name=pieces[0],
                                             total_pieces=len(pieces),
-                                                tutorial=1)
+                                                tutorial=1,
+                                              sever_name=sever_name)
 
 @app.route('/evaluate/<int:piece_number>')
 def evaluate(piece_number):
@@ -50,14 +60,16 @@ def evaluate(piece_number):
         return render_template('evaluate.html', piece_number=piece_number,
                                                   piece_name=pieces[piece_number],
                                                 total_pieces=len(pieces),
-                                                    tutorial=0)
+                                                    tutorial=0,
+                                                  sever_name=sever_name)
     else:
         abort(404)
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html', piece_number=0,
-                                           total_pieces=len(pieces))
+                                           total_pieces=len(pieces),
+                                           sever_name=sever_name)
 
 @app.route('/end')
 def end():
